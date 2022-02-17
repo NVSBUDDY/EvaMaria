ccab0d84be852f98ea8bf596b6ccc452056332a9
hiren patel <89856028+ghostediting@users.noreply.github.com>
2022-02-17 16:31:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -100,7 +100,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('⚠ START ME ⚠', url='https://t.me/NVS_FILTER_28_bot')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
