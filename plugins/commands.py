d6f4d0490282ac4131ec88100f049503f1e3b6d3
hiren patel <89856028+ghostediting@users.noreply.github.com>
2022-03-06 20:24:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,11 +42,11 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton(' 🔍 Search Here  ', switch_inline_query_current_chat='')
+        ],[
+            InlineKeyboardButton('🔍 Movie log', url='https://t.me/NVSHDMOVIELOG'),
             InlineKeyboardButton('🤖 Channel', url='https://t.me/NVSHDMOVIE')
-            ],[
+        ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
